341c7101b414d73eb66471f75b536be2696b2dfd
Ognjen Stefanovic <ognjen.stefanovic@codaxy.com>
2022-07-16 23:06:03 +0200

    Connect server and client

---
+++ server/Server.Api/Pages/App.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Server.Api.Pages;
+
+public class App
+{
+    public class DefaultModel : PageModel
+    {
+
+    }
+}
+
---
+++ server/Server.Api/Utilities/DebugUtil.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Server.Api.Utilities;
+
+public class DebugUtil
+{
+#if DEBUG
+    public static bool IsDebugMode => true;
+#else
+    public static bool IsDebugMode => false;
+#endif
+}
